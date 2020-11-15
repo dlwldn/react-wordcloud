fba90e2e872f2c3c8f08cb59a4303df30555b553
unknown <00ljw00@naver.com>
2020-11-15 19:36:25 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppShell from './components/AppShell'
+
+function App() {
+  return (
+    <div>
+      <AppShell></AppShell>
+    </div>
+  );
+}
+
+export default App;
