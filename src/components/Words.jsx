c31da1b9a349353da8a0ce9ad08b24a74d301ddc
unknown <00ljw00@naver.com>
2020-11-17 00:48:39 +0900

    리액트 라우터 추가

---
+++ src/components/Words.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+class Words extends Component {
+    render() {
+        return (
+            <Card>
+                <CardContent>
+                    Words 페이지
+                </CardContent>
+            </Card>
+        )
+    }
+}
+
+export default Words;
