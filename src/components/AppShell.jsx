b4e026a8bfdd54b10ad3ee7991f73baabe28cdbf
unknown <00ljw00@naver.com>
2020-11-17 18:18:16 +0900

    추가 삭제 기능구현

--- src/components/AppShell.jsx
+++ src/components/AppShell.jsx
@@ -45,17 +45,17 @@ class AppShell extends Component {
                     <Drawer open={this.state.toggle}>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to="/">
-                                Home
+                                홈 화면
                             </Link>
                         </MenuItem>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to="/texts">
-                                Texts
+                                텍스트 관리
                             </Link>
                         </MenuItem>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to="/words">
-                                Words
+                                단어 관리
                             </Link>
                         </MenuItem>
                     </Drawer>
